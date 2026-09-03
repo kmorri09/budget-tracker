804c75fbb8263c88e312fa01151a58cc9dc5befd
kmorri09 <kevin@ruoomsoftware.com>
2026-09-03 11:27:27 -0500

    Add branded budget favicon

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,6 +9,6 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#f7f8f6",
     theme_color: "#f7f8f6",
-    icons: [],
+    icons: [{ src: "/icon.svg", sizes: "any", type: "image/svg+xml", purpose: "any" }],
   };
 }
